62d6b2d2ecf03521d1938694957c75a29481afa6
BRASNICA041 <BRASNICA041@samba.brasnica.com.br>
2025-08-04 07:19:43 -0300

    Limpeza do projeto e configuração inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold text-pink-600 underline">
+        Olá, meu casamento!
+      </h1>
     </div>
   );
 }
